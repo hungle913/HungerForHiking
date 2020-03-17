66cb4f4fb37dd05a411d063fc18c63ef43cd8f26
jvanhook13 <jaredvanhook1313@gmail.com>
2020-03-16 20:59:30 -0700

    Now icorporates with trail api and saves the lat long properly as variables

--- assets/javascript/jared_code.js
+++ assets/javascript/jared_code.js
@@ -11,13 +11,13 @@
 
 //First we intialize firebase
 var firebaseConfig = {
-  apiKey: "AIzaSyAS69rSyD2JKuSdzXh4MyuMgZOm9IHN9tQ",
-  authDomain: "hunger-for-hiking.firebaseapp.com",
-  databaseURL: "https://hunger-for-hiking.firebaseio.com",
-  projectId: "hunger-for-hiking",
-  storageBucket: "hunger-for-hiking.appspot.com",
-  messagingSenderId: "1072732894647",
-  appId: "1:1072732894647:web:79aedcb9e81e6a2b7fa432"
+    apiKey: "AIzaSyAS69rSyD2JKuSdzXh4MyuMgZOm9IHN9tQ",
+    authDomain: "hunger-for-hiking.firebaseapp.com",
+    databaseURL: "https://hunger-for-hiking.firebaseio.com",
+    projectId: "hunger-for-hiking",
+    storageBucket: "hunger-for-hiking.appspot.com",
+    messagingSenderId: "1072732894647",
+    appId: "1:1072732894647:web:79aedcb9e81e6a2b7fa432"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -66,31 +66,110 @@ $("#submit").on("click", event => {
             const DB = snapshot.val()
 
 
-            DB.filter(info => {
-                console.log("info" , info)
-                if (info.includes(zipcode)) {
-                    
+            const result = DB.filter(info => {
+                    console.log("info", info)
+                    if (info.includes(zipcode)) {
 
-                    var lat = info[1]
-                    var lng = info[2]
-                    console.log(info, "info")
-                    console.log(lat, "lat")
-                    console.log(lng, "lng")
-                    result ;
-                    console.log(result , "result")
-                    
 
-                } else {
+                        var lat = info[1]
+                        var lng = info[2]
+                        var result = [lat, lng]
+                        console.log(info, "info")
+                        console.log(lat, "lat")
+                        console.log(lng, "lon")
 
-                    console.log("better luck next time")
+                        console.log(result, "result")
+
+
+                        var apiKey = "200702695-d743746acbe4331b82eeffe769ec073d";
+
+                        // create a GET function and input
+
+                        var form = new FormData();
+                        var settings = {
+                            url: "https://www.hikingproject.com/data/get-trails?lat=" +
+                                lat +
+                                "&lon=" +
+                                lng +
+                                "&maxDistance=10&key=" +
+                                apiKey,
+                            method: "GET"
+                            // success: function(result) {
+                            // console.log(result);
+                            // },
+                            // error: function(error) {
+                            // console.log(error);
+                            // }
+                        };
+
+                        // create variable for API
+                        $.ajax(settings).then(function (response) {
+                            // console.log(response);
+                            // console.log(response.trails[1].name);
+
+                            // loop through results for the functional logic
+                            for (var i = 0; i < response.trails.length; i++) {
+                                // console.log(response.trails[i].imgSmallMed);
+                                // console.log(response.trails[i].name);
+                                // console.log(response.trails[i].length);
+                                // console.log(response.trails[i].difficulty);
+                                // console.log(response.trails[i].ascent);
+                                // console.log(response.trails[i].descent);
+                                // console.log(console.log(response.trails[i].url));
+
+                                // append variables to div with id="trail-card"
+
+                                var trailCard = $("<div>");
+                                // trailCard.setAttribute("trail-id", i);
+                                trailCard.append(
+                                    "<img class='card-image' src= '" + response.trails[i].imgSmallMed + "'>"
+                                );
+                                trailCard.append("<p class='title'>" + response.trails[i].name + "</p>");
+                                trailCard.append(
+                                    "<p class='details'>Length: " + response.trails[i].length + " miles</p>"
+                                );
+                                trailCard.append(
+                                    "<p class='details'>Difficulty: " +
+                                    response.trails[i].difficulty +
+                                    "</p>"
+                                );
+                                trailCard.append(
+                                    "<p class='details'>Ascent: " + response.trails[i].ascent + "</p>"
+                                );
+                                trailCard.append(
+                                    "<p class='details'>Descent: " + response.trails[i].descent + "</p>"
+                                );
+                                trailCard.append(
+                                    "<p class='details'><a href='" +
+                                        response.trails[i].url +
+                                    "' target='_blank'>More Info</a></p>"
+                                );
+
+                                // append trail card to "resultsTrails"
+
+                                $("#resultsTrails").append(trailCard);
+                            }
+                        });
+                    };
+
 
 
                 }
 
- 
+                //zomato api
 
-            })
+
+                //weather api
+
+
+
+            )
+            console.log(result, "result fancy")
+
+
+
+
+
         });
-    });
-})
-
+    })
+});
